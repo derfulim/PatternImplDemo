92640de8376bf6ae3878a54f810a1d6c7f00d9ce
derfulim <barashenko@ukr.net>
2020-01-14 20:36:25 +0200

    Added fields for tour class

---
+++ src/builder/Tour.java
@@ -0,0 +1,13 @@
+package builder;
+
+public class Tour {
+
+   boolean isTransferToAirportIncluded;
+   boolean isTransferFromAirportIncluded;
+   boolean isFlightToResortIncluded;
+   boolean isFlightFromResortIncluded;
+   boolean isBookingIncluded;
+   boolean isInsuranceIncluded;
+
+
+}
